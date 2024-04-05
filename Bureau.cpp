fdde2156dfa8773bf85a242b710998f369514207
Ziad Ijja <ziad.ijja@etu.univ-nantes.fr>
2024-04-05 10:41:04 +0200

    wsh javais oublié

--- Bureau.cpp
+++ Bureau.cpp
@@ -64,129 +64,175 @@ int Bureau::choisirIsoloirDisponible() {
     return 0 ;
 }
 
+void Bureau::entrerElecteursDansFile(int temps, int &indiceElecteur)
+{
+
+    if (temps <= (int)Parametrage::TEMPS_MAX && temps % 2 == 1 && indiceElecteur < (int)getListeElectorale().getListeElectorale().size())
+    {
+        // random pour la proba d'abstention
+        std::random_device seed;
+        std::mt19937 gen(seed());
+
+        std::uniform_int_distribution<size_t> AbstentionChance(0, 100);
+        const double randValeur = (float)AbstentionChance(gen) / 100; // donne une val entre 0 et 1
+        ElecteurEngage *electeurBureau = getListeElectorale().getElecteurIndex(indiceElecteur++);
+
+        if (randValeur > Parametrage::PROBABILITE_ABSTENTION)
+        {
+            std::cout << "  ENTREE\n       " << electeurBureau->getId() << " entre" << std::endl;
+            p_fileBureauVersTableDecharge.push(electeurBureau);
+        }
+        else
+        {
+            std::cout << electeurBureau->getId() << " entre pas " << std::endl;
+            ;
+        }
+    }
+}
+
+
+void Bureau::traiterTableDecharge(ElecteurEngage *&electeur)
+{
+    if (!p_fileBureauVersTableDecharge.empty() || !getTableDecharge().estVide())
+    {
+
+        // lorsque la table est vide
+        if (getTableDecharge().estVide())
+        {
+            std::cout << "  DECHARGE" << std::endl;
+            electeur = p_fileBureauVersTableDecharge.front();
+            p_fileBureauVersTableDecharge.pop();
+            std::cout << "       " << electeur->getId() << " entre" << std::endl;
+            getTableDecharge().entrerTableDecharge(electeur);
+
+            // si elle est déjà utilisée, on décrémente simplement
+        }
+        else
+        {
+            electeur = getTableDecharge().getOccupant();
+            electeur->decrementeTempsRestant();
+
+            if (electeur->getTempsRestant() == 0)
+            {
+                getTableDecharge().choisirBulletins();
+                std::cout << "  DECHARGE" << std::endl;
+                for (const auto &b : electeur->getBulletins())
+                {
+                    std::cout << "       " << electeur->getId() << " prend " << b.nomCandidat << std::endl;
+                }
+            }
+            if (electeur->getTempsRestant() <= -1)
+            {
+                getTableDecharge().sortirTableDecharge(p_fileTableDechargeVersIsoloires);
+                std::cout << "  DECHARGE\n       " << electeur->getId() << " sort" << std::endl;
+            }
+        }
+    }
+}
+
+void Bureau::traiterIsoloires(ElecteurEngage *&electeur)
+{
+
+    int indiceIsoloire = choisirIsoloirDisponible();
+
+    if (!p_fileTableDechargeVersIsoloires.empty() || !getIsoloire(indiceIsoloire).estVide())
+    {
+        if (indiceIsoloire != -1 && getIsoloire(indiceIsoloire).estVide())
+        {
+            std::cout << "  ISOLOIR" << std::endl;
+            electeur = p_fileTableDechargeVersIsoloires.front();
+            p_fileTableDechargeVersIsoloires.pop();
+            std::cout << "       " << electeur->getId() << " entre" << std::endl;
+            getIsoloire(indiceIsoloire).entrerIsoloire(electeur);
+            getIsoloire(indiceIsoloire).choisirBulletinFinal();
+        }
+    }
+
+    for (int i = 0; i < (int)Parametrage::NOMBRE_ISOLOIRS; ++i)
+    {
+
+        if (!getIsoloire(i).estVide())
+        {
+
+            getIsoloire(i).getOccupant()->decrementeTempsRestant();
+            if (getIsoloire(i).getOccupant()->getTempsRestant() == 0)
+            {
+
+                if (!getIsoloire(i).estVide())
+                {
+                    std::cout << "  ISOLOIR\n       " << getIsoloire(i).getOccupant()->getId() << " prend "
+                              << getIsoloire(i).getOccupant()->getBulletinFinal().nomCandidat << std::endl;
+                }
+            }
+
+            if (getIsoloire(i).getOccupant()->getTempsRestant() <= -1)
+            {
+                std::cout << "  ISOLOIR" << std::endl;
+                std::cout << "       " << getIsoloire(i).getOccupant()->getId() << " sort" << std::endl;
+                getIsoloire(i).sortirIsoloire(p_fileIsoloiresVersTableVote);
+            }
+        }
+    }
+}
+
+void Bureau::traiterTableVote(ElecteurEngage *&electeur)
+{
+
+    if (!p_fileIsoloiresVersTableVote.empty() || !getTableVote().estVide())
+    {
+
+        if (getTableVote().estVide())
+        {
+
+            std::cout << "  VOTE" << std::endl;
+            electeur = p_fileIsoloiresVersTableVote.front();
+            p_fileIsoloiresVersTableVote.pop();
+
+            std::cout << "       " << electeur->getId() << " entre" << std::endl;
+            getTableVote().entrerTableVote(electeur);
+        }
+        else
+        {
+            electeur = getTableVote().getOccupant();
+            electeur->decrementeTempsRestant();
+
+            if (electeur->getTempsRestant() == 0)
+            {
+
+                std::cout << "  VOTE\n       " << electeur->getId() << " vote" << std::endl;
+                getTableVote().placerBulletin();
+                getTableVote().emarger();
+            }
+            if (electeur->getTempsRestant() <= -1)
+            {
+                std::cout << "  VOTE\n       " << electeur->getId() << " sort" << std::endl;
+                getTableVote().sortirTableVote();
+            }
+        }
+    }
+}
+
+
 void Bureau::main(int & temps, int & indiceElecteur) {
+
+    ElecteurEngage *electeur;
 
     std::cout << "T = " << temps << std::endl ;
     // augmentation du temps
     temps++;
 
-    // entrer les électeurs dans la file 
-    if (temps <= (int)Parametrage::TEMPS_MAX && temps % 2 == 1 && indiceElecteur < (int)getListeElectorale().getListeElectorale().size()) {
-        
-       // random pour la proba d'abstention 
-        std::random_device seed;
-        std::mt19937 gen(seed());
+    // entrer les électeurs dans la file
+    entrerElecteursDansFile(temps, indiceElecteur);
 
-        // pour l'abstention
-        std::uniform_int_distribution<size_t> AbstentionChance(0, 100);
-        const double randValeur = (float) AbstentionChance(gen) / 100 ; // donne une val entre 0 et 1
-        ElecteurEngage* electeurBureau = getListeElectorale().getElecteurIndex(indiceElecteur++);
-
-        if (randValeur > Parametrage::PROBABILITE_ABSTENTION){
-            std::cout << "  ENTREE\n       "<< electeurBureau->getId() << " entre" << std::endl ;
-            p_fileBureauVersTableDecharge.push(electeurBureau);
-        } else {
-            std::cout << electeurBureau->getId() << " entre pas " << std::endl ;;
-        }
-        
-    }
-
-    ElecteurEngage* electeur ;
     // bureau vers table de decharge
-    if (!p_fileBureauVersTableDecharge.empty() || !getTableDecharge().estVide()) {
-            
-        // lorsque la table est vide
-        if (getTableDecharge().estVide()) {
-            std::cout << "  DECHARGE" << std::endl ;
-            electeur = p_fileBureauVersTableDecharge.front();
-            p_fileBureauVersTableDecharge.pop();      
-            std::cout << "       "<< electeur->getId() << " entre" << std::endl ;
-            getTableDecharge().entrerTableDecharge(electeur);
-                        
-        // si elle est déjà utilisée, on décrémente simplement
-        } else {     
-            electeur = getTableDecharge().getOccupant();
-            electeur->decrementeTempsRestant();
-
-            if (electeur->getTempsRestant() == 0) {
-                getTableDecharge().choisirBulletins();
-                std::cout << "  DECHARGE" << std::endl ;
-                for (const auto& b : electeur->getBulletins()){
-                    std::cout << "       " << electeur->getId() << " prend " << b.nomCandidat << std::endl;
-                }
-                
-            }
-            if (electeur->getTempsRestant() <= -1) {
-                getTableDecharge().sortirTableDecharge(p_fileTableDechargeVersIsoloires);
-                std::cout << "  DECHARGE\n       "<< electeur->getId() << " sort" << std::endl ;
-            }
-        }
-    }        
+    traiterTableDecharge(electeur);
 
     // table de decharge vers isoloires
-    int indiceIsoloire = choisirIsoloirDisponible();    
-    if (!p_fileTableDechargeVersIsoloires.empty() || getIsoloire(indiceIsoloire).estVide() == false) {
-        if (indiceIsoloire != -1 && getIsoloire(indiceIsoloire).estVide()) {       
-            std::cout << "  ISOLOIR" << std::endl ;
-            electeur = p_fileTableDechargeVersIsoloires.front();
-            p_fileTableDechargeVersIsoloires.pop();         
-            std::cout << "       "<< electeur->getId() << " entre" << std::endl ;
-            getIsoloire(indiceIsoloire).entrerIsoloire(electeur);
-            getIsoloire(indiceIsoloire).choisirBulletinFinal();       
-        }          
-    }
+    traiterIsoloires(electeur);
 
-    for (int i = 0 ; i < (int)Parametrage::NOMBRE_ISOLOIRS ; ++i) {
+    // isoloires vers table de vote
+    traiterTableVote(electeur);
 
-            if (!getIsoloire(i).estVide()) {
-
-                getIsoloire(i).getOccupant()->decrementeTempsRestant();                
-                if (getIsoloire(i).getOccupant()->getTempsRestant() == 0) {
-                            
-                    if (!getIsoloire(i).estVide()){
-                        std::cout << "  ISOLOIR\n       " << getIsoloire(i).getOccupant()->getId() << " prend " << getIsoloire(i).getOccupant()->getBulletinFinal().nomCandidat << std::endl;
-                        
-                    }
-                }
-
-                if (getIsoloire(i).getOccupant()->getTempsRestant() <= -1) {
-                    std::cout << "  ISOLOIR" << std::endl ;
-                    std::cout << "       " << getIsoloire(i).getOccupant()->getId() << " sort" << std::endl ;
-                    getIsoloire(i).sortirIsoloire(p_fileIsoloiresVersTableVote);
-                }
-            }   
-        }
-
-    // isoloires vers table de vote    
-    if(!p_fileIsoloiresVersTableVote.empty() || !getTableVote().estVide()) {
-
-        if (getTableVote().estVide()) {
-
-            std::cout << "  VOTE" << std::endl ;
-            electeur = p_fileIsoloiresVersTableVote.front();
-            p_fileIsoloiresVersTableVote.pop();
-
-            std::cout << "       "<< electeur->getId() << " entre" << std::endl ;
-            getTableVote().entrerTableVote(electeur);
-            
-        } else {
-            electeur = getTableVote().getOccupant();
-            electeur->decrementeTempsRestant();
-
-            if (electeur->getTempsRestant() == 0) {
-
-                std::cout << "  VOTE\n       " << electeur->getId() << " vote" << std::endl;
-                getTableVote().placerBulletin();
-                getTableVote().emarger();
-
-                
-            }
-            if (electeur->getTempsRestant() <= -1) {
-                std::cout << "  VOTE\n       " << electeur->getId() << " sort" << std::endl;
-                getTableVote().sortirTableVote();
-            }
-        } 
-    }
 }
 
 
